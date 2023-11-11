a68acca6d2fd1495e57a3fa8a249c2172ca5be9e
Sirraide <aeternalmail@gmail.com>
2023-11-11 20:45:30 +0100

    [CMake] Allow specifying test dependencies

--- cmake/FCHKDiscoverTests.cmake
+++ cmake/FCHKDiscoverTests.cmake
@@ -1,7 +1,9 @@
 enable_testing()
 
 ## User or the projects CMake file is expected to provide this.
-set(FCHK_EXE_PATH "fchk" CACHE FILEPATH "Path to the fchk executable" )
+if (NOT DEFINED FCHK_EXE_PATH)
+    set(FCHK_EXE_PATH "fchk" CACHE FILEPATH "Path to the fchk executable" )
+endif()
 
 ## Collect tests in one or more directories.
 ##
@@ -13,6 +15,7 @@ set(FCHK_EXE_PATH "fchk" CACHE FILEPATH "Path to the fchk executable" )
 ##   WORKING_DIRECTORY: The working directory to use for the test.
 ##   ARGS: Additional arguments to pass to fchk.
 ##   RECURSIVE: If set, recurse into subdirectories.
+##   DEPENDS: Mark that the tests depend on one or more targets.
 ##
 ## Example:
 ##   FCHKAddAllTestsInDir(
@@ -26,7 +29,7 @@ function(FCHKAddAllTestsInDir)
 
     set(options RECURSIVE)
     set(oneValueArgs TEST_NAME_PREFIX PREFIX WORKING_DIRECTORY)
-    set(multiValueArgs IN PATTERN ARGS)
+    set(multiValueArgs IN PATTERN ARGS DEPENDS)
     cmake_parse_arguments(FCHKAddAllTestsInDir
         "${options}"
         "${oneValueArgs}"
@@ -38,6 +41,26 @@ function(FCHKAddAllTestsInDir)
     if (FCHKAddAllTestsInDir_PREFIX)
         set(fchk_prefix --prefix ${FCHKAddAllTestsInDir_PREFIX})
     endif()
+
+    ## Ungodly jank to allow people to add dependencies on other targets to the tests.
+    if (FCHKAddAllTestsInDir_DEPENDS)
+        foreach (dependency ${FCHKAddAllTestsInDir_DEPENDS})
+            if (NOT TEST "_fchk_build_deps-${dependency}")
+                add_test(
+                    NAME "_fchk_build_deps-${dependency}"
+                    COMMAND "${CMAKE_COMMAND}"
+                    --build "${CMAKE_BINARY_DIR}"
+                    --config "$<CONFIG>"
+                    --target "${dependency}"
+                    COMMAND_EXPAND_LISTS
+                )
+
+                set_tests_properties("_fchk_build_deps-${dependency}"
+                    PROPERTIES FIXTURES_SETUP _fchk_build_deps
+                )
+            endif()
+        endforeach()
+   endif()
 
     foreach (dir ${FCHKAddAllTestsInDir_IN})
         foreach (pat ${FCHKAddAllTestsInDir_PATTERN})
@@ -52,6 +75,12 @@ function(FCHKAddAllTestsInDir)
                         ${test}
                     WORKING_DIRECTORY ${FCHKAddAllTestsInDir_WORKING_DIRECTORY}
                 )
+
+                if (FCHKAddAllTestsInDir_DEPENDS)
+                    set_tests_properties("${FCHKAddAllTestsInDir_TEST_NAME_PREFIX}${test}"
+                        PROPERTIES FIXTURES_REQUIRED _fchk_build_deps
+                    )
+                endif()
             endforeach()
         endforeach()
     endforeach()
