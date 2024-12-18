6c0aa6ecf76c204a7b0a82bc231a9900ce56c73b
Sirraide <aeternalmail@gmail.com>
2024-12-18 19:54:57 +0100

    [Refactor] Remove unused variable

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,5 @@
 using namespace fchk;
 
 int main(int argc, char** argv) {
-    auto dh = std::make_shared<DiagsHandler>();
     return Context::RunMain(std::make_shared<DiagsHandler>(), argc, argv);
 }
